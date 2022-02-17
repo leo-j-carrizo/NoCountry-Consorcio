bcd954765e24a8ae06f9c42738d70e5ebc6eeeaf
LZDEV <81957303+Lestradez@users.noreply.github.com>
2022-02-17 12:30:10 -0300

    Feat: sass, router, app structure

---
+++ client/src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+//import Landing from '../components/Landing';
+
+
+export default function Home() {
+  
+return (
+    <div style={{width:'100%', minHeight:'100vh'}}>
+      
+        HOME
+    
+    </div>
+  );
+}
